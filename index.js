b393882ed97d79bab0f4b47f59ac5676f8b7cf7e
yahyacodes <yahya.codin@gmail.com>
2023-09-18 14:59:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  let result;
+  if (ride <= 400) {
+    result = "This one is on me!";
+  } else if (ride > 2000 && ride <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (ride > 2500) {
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch (tip) {
+    case "generous":
+      result = "Thank you so much.";
+      break;
+    case "not as generous":
+      result = "Thank you.";
+      break;
+
+    default:
+      result = "Bye.";
+      break;
+  }
+  return result;
 }
